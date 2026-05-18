3047ae24887ffda3da799b2dba639cac7a2a1bff
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-05-18 15:34:14 -0400

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,10 +4,9 @@ fn main(){
     let mut s = System::new_with_specifics(
     RefreshKind::everything().with_cpu(CpuRefreshKind::everything()),
 );
-/ 
+
     // Wait a bit because CPU usage is based on diff.
     std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
-    // Refresh CPUs again to get actual value.
     s.refresh_all();
 
     for cpu in s.cpus() {
